630a76884e3a9047da9bd6a5c142eb0fb9a39bdd
unknown <Administrator@.(none)>
2012-01-11 18:14:04 +0200

    report event initial implementation

--- src/infi/logging/win32.py
+++ src/infi/logging/win32.py
@@ -1,6 +1,18 @@
 from ctypes import *
 from infi.registry import *
 
+RegisterEventSourceW = windll.advapi32.RegisterEventSourceW
+DeregisterEventSource = windll.advapi32.DeregisterEventSource
+ReportEventW = windll.advapi32.ReportEventW
+
+# From http://msdn.microsoft.com/en-us/library/windows/desktop/aa363679%28v=VS.85%29.aspx
+EVENTLOG_SUCCESS = 0x0000 
+EVENTLOG_AUDIT_FAILURE = 0x0010 
+EVENTLOG_AUDIT_SUCCESS = 0x0008 
+EVENTLOG_ERROR_TYPE = 0x0001 
+EVENTLOG_INFORMATION_TYPE = 0x0004 
+EVENTLOG_WARNING_TYPE = 0x0002 
+
 def register_application(app_name, message_file_path):
     """Registers an application as an event log source so it can later create events.
     
@@ -19,3 +31,19 @@ def unregister_application(app_name):
     """Unregisters an application as a log source by removing it from the registry."""
     event_log = LocalComputer().local_machine['SYSTEM']['CurrentControlSet']['Services']['Eventlog']
     del event_log['Application'][app_name]
+
+def register_event_source(app_name):
+    handle = RegisterEventSourceW(None, unicode(app_name))
+    if handle == 0:
+        raise WinError()
+    return handle
+
+def deregister_event_source(handle):
+    DeregisterEventSource(handle)
+
+def report_event(handle, type, category, event_id, strings, raw_data):
+    raw_data_len = 0 if raw_data is None else len(raw_data)
+    strings_ptr = (c_wchar_p * len(strings))()
+    for i in xrange(len(strings)):
+        strings_ptr[i] = c_wchar_p(unicode(strings[i]))
+    return ReportEventW(handle, type, category, event_id, None, len(strings), raw_data_len, strings_ptr, raw_data)
--- tests/test_win32.py
+++ tests/test_win32.py
@@ -4,6 +4,32 @@ from infi.logging.win32 import *
 class Win32TestCase(TestCase):
     def test_register_app(self):
         try:
-            register_application("test_app", r"C:\none")
+            register_application("test_app", self._message_file_path())
         finally:
             unregister_application("test_app")
+
+    def test_register_event_source(self):
+        try:
+            register_application("test_app", self._message_file_path())
+            handle = register_event_source("test_app")
+            self.assertNotEqual(handle, 0)
+        finally:
+            unregister_application("test_app")
+
+    def test_report_event(self):
+        try:
+            register_application("test_app", self._message_file_path())
+            handle = register_event_source("test_app")
+            result = report_event(handle, EVENTLOG_SUCCESS, 0, 0, [ "testing 123" ], None)
+            print(result)
+        finally:
+            deregister_event_source(handle)
+            unregister_application("test_app")
+
+    def _message_file_path(self):
+        from os import path
+        import sys
+
+        assets_dir = path.abspath(path.join(path.dirname(__file__), "..", "assets"))
+        arch = "amd64" if sys.maxsize > (1 << 31) else "x86"
+        return path.join(assets_dir, arch, "messages.dll")
