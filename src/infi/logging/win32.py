2a106de729da21647c12cd08dcbcc8b12b56d409
Tal Yalon <yalon@infinidat.com>
2014-07-15 16:16:48 +0300

    TRIVIAL PEP8

--- src/infi/logging/win32.py
+++ src/infi/logging/win32.py
@@ -15,6 +15,7 @@ EVENTLOG_ERROR_TYPE = 0x0001
 EVENTLOG_INFORMATION_TYPE = 0x0004
 EVENTLOG_WARNING_TYPE = 0x0002
 
+
 def register_application(app_name, message_file_path=None):
     """Registers an application as an event log source so it can later create events.
 
@@ -34,12 +35,14 @@ def register_application(app_name, message_file_path=None):
     event_log['Application'][app_name] = None
     app_key = event_log['Application'][app_name]
     app_key.values_store['EventMessageFile'] = value_factory.by_value(message_file_path)
-    app_key.values_store['TypesSupported'] = value_factory.by_value(7) # All severity types
+    app_key.values_store['TypesSupported'] = value_factory.by_value(7)  # All severity types
+
 
 def unregister_application(app_name):
     """Unregisters an application as a log source by removing it from the registry."""
     event_log = LocalComputer().local_machine['SYSTEM']['CurrentControlSet']['Services']['Eventlog']
     event_log['Application'].pop(app_name, None)
+
 
 def register_event_source(app_name):
     """Registers an event source and returns an open handle. Raises WinError on error."""
@@ -48,9 +51,11 @@ def register_event_source(app_name):
         raise WinError()
     return handle
 
+
 def deregister_event_source(handle):
     """Closes an open event source handle."""
     DeregisterEventSource(handle)
+
 
 def report_event(handle, type, category, event_id, strings, raw_data):
     """Reports an event to Windows event log. Raises WinError on error.
@@ -71,6 +76,7 @@ def report_event(handle, type, category, event_id, strings, raw_data):
     result = ReportEventW(handle, type, category, event_id, None, len(strings), raw_data_len, strings_ptr, raw_data)
     if result != 1:
         raise WinError()
+
 
 class Win32EventLogHandler(Handler, StringFormatterHandlerMixin):
     default_format_string = "{record.message}"
@@ -128,4 +134,4 @@ class Win32EventLogHandler(Handler, StringFormatterHandlerMixin):
         type = self.get_event_type(record)
         message = self.format(record)
         raw_data = self.get_raw_data(record)
-        report_event(self._win32_handle, type, cat, id, [ message ], raw_data)
+        report_event(self._win32_handle, type, cat, id, [message], raw_data)
